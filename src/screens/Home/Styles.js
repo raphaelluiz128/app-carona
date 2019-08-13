42c14a99e2306b68cd3c7c9c74a276c13514dc07
Evandro Ribeiro <heppierservices@gmail.com>
2019-08-12 21:48:05 -0300

    Refactoring Subtitle Component to Inherit Title Component Properties

--- src/screens/Home/Styles.js
+++ src/screens/Home/Styles.js
@@ -1,73 +1,74 @@
-import styled from 'styled-components/native';
-import commonStyles from '../../commonStyles';
+import styled from "styled-components/native";
+import commonStyles from "../../commonStyles";
 
 export const Container = styled.View`
-flex:1;`
+  flex: 1;
+`;
 
 export const Background = styled.View`
-flex:3;`
+  flex: 3;
+`;
 
 export const TitleBar = styled.View`
-flex:1;
-justify-content: flex-end;`
+  flex: 1;
+  justify-content: flex-end;
+`;
 
 export const Title = styled.Text`
-    font-family: ${commonStyles.fontFamily};
-    color: ${commonStyles.colors.secondary};
-    font-size: 30;
-    margin-left: 20;
-    margin-bottom: 10;
-`
+  font-family: ${commonStyles.fontFamily};
+  color: ${commonStyles.colors.secondary};
+  font-size: 30;
+  margin-left: 20;
+  margin-bottom: 10;
+`;
 
-export const SubTitle = styled.Text`
-    font-family: ${commonStyles.fontFamily};
-    color: ${commonStyles.colors.secondary};
-    font-size: 20;
-    margin-left: 20;
-    margin-bottom: 30;
-`
+export const SubTitle = styled(Title)`
+  font-size: 20;
+  margin-left: 20;
+  margin-bottom: 30;
+`;
 
 export const Login = styled.View`
-    flex: 2;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-    margin-top: 26;
-`
+  flex: 2;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 26;
+`;
 
 export const loginOk = styled.View`
-    flex: 2;
-    flex-direction: column;
-    align-items: center;
-    justify-content:center;
-    margin-top: 60;
-`
+  flex: 2;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 60;
+`;
 
 export const AvisoLoginOk = styled.Text`
-    font-family: ${commonStyles.fontFamily};
-    color: ${commonStyles.colors.secondary};
-    font-size: 18;
-    margin-top: 15;
-`
+  font-family: ${commonStyles.fontFamily};
+  color: ${commonStyles.colors.secondary};
+  font-size: 18;
+  margin-top: 15;
+`;
 
 export const TextLogin = styled.TextInput`
-    font-family: ${commonStyles.fontFamily};
-    color: ${commonStyles.colors.secondary};
-    font-size: 18;
-    border-color: gray;
-    border-bottom-width: 1;
-`
+  font-family: ${commonStyles.fontFamily};
+  color: ${commonStyles.colors.secondary};
+  font-size: 18;
+  border-color: gray;
+  border-bottom-width: 1;
+`;
 
 export const TextLoginOk = styled.Text`
-    font-family: ${commonStyles.fontFamily};
-    color: ${commonStyles.colors.secondary};
-    font-size: 22;
-`
+  font-family: ${commonStyles.fontFamily};
+  color: ${commonStyles.colors.secondary};
+  font-size: 22;
+`;
 
 export const TaksContainer = styled.View`
-    flex: 5;
-    marginTop: 35;
-    flexDirection:row;
-    alignItems: center;
-    justifyContent: space-around;   
-`
+  flex: 5;
+  margintop: 35;
+  flexdirection: row;
+  alignitems: center;
+  justifycontent: space-around;
+`;
